398b0e47d3a1bbc4df4cb639ee7b304e801bacfd
Yena Baek <yena2324@gmail.com>
2024-03-12 18:10:05 +0900

    feat. 결제관련 div에 따라 결제금액 relative에서 top으로 변경,style. 결제와 개인정보 ui 구성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,38 +16,39 @@ import { Input } from '@/components/ui/input';
 import { ChevronDown, X } from 'lucide-react';
 import { Checkbox } from '@/components/ui/checkbox';
 import { Badge } from '@/components/ui/badge';
-import { CornerDownRight } from 'lucide-react';
-// import { cn } from '@/lib/utils';
-// import { useEffect, useRef, useState } from 'react';
+import kakaoPay from '../../public/kakaoPay.png';
+import naverPay from '../../public/naverPay.png';
+import tossPay from '../../public/tossPay.png';
+import { useEffect, useRef, useState } from 'react';
+import { cn } from '@/lib/utils';
 
 export default function Home() {
-  //   const [isSticky, setIsSticky] = useState(false);
-  //   const ref = useRef<HTMLDivElement>(null);
+  const totalDivRef = useRef<HTMLDivElement | null>(null);
+  const [IsTotalRef, setIsTotalRef] = useState(false);
 
-  //   useEffect(() => {
-  //     const handleScroll = () => {
-  //       if (ref.current) {
-  //         // 스크롤 위치가 요소의 상단보다 아래에 있으면 relative로 유지
-  //         if (window.scrollY < ref.current.offsetTop) {
-  //           setIsSticky(false);
-  //         } else {
-  //           // 스크롤 위치가 요소의 상단을 넘어가면 fixed로 변경
-  //           setIsSticky(true);
-  //         }
-  //       }
-  //     };
+  // totalDivRef가 페이지 맨 위에 닿았을 때 relative => fixed 로 변환
+  const onScroll = () => {
+    if (totalDivRef.current) {
+      const totalDivTop = totalDivRef.current.getBoundingClientRect().top;
+      if (totalDivTop <= 0) {
+        setIsTotalRef(true);
+      } else {
+        setIsTotalRef(false);
+      }
+    }
+  };
 
-  //     window.addEventListener('scroll', handleScroll);
+  useEffect(() => {
+    onScroll();
+    window.addEventListener('scroll', onScroll);
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [IsTotalRef]);
 
-  //     return () => {
-  //       window.removeEventListener('scroll', handleScroll);
-  //     };
-  //   }, []);
   return (
     <div>
       <div className="flex flex-col items-center gap-[90px]">
-        {/* 주문페이지 추가할 지 고민하기 */}
-
         <div className="text-4xl font-semibold mt-[85px]">결제하기</div>
         <div className="grid p-0 w-[1050px] gap-[0px]">
           {/* 왼쪽 div */}
@@ -125,157 +126,147 @@ export default function Home() {
               </CardContent>
             </Card>
           </div>
+          <div ref={totalDivRef}>
+            <div className="flex border-t-[2px] border-gray-200/15 pt-[50px]">
+              <div className="grid justify-start justify-items-center w-[742px] pb-[300px]">
+                <Card className="border-none h-[500px]">
+                  <CardTitle className="text-[20px] pb-[10px]">쿠폰/포인트</CardTitle>
+                  <CardContent className="flex gap-[40px] w-[700px] border-black border-t-2 px-[5px] py-[40px]">
+                    <label>쿠폰/포인트</label>
+                    <div className="grid gap-[20px]">
+                      <div>
+                        <p className="mb-[10px]">쿠폰</p>
+                        <Button className="hidden">
+                          <X />
+                        </Button>
+                        <div className="flex gap-[8px] w-[480px]">
+                          <Input className=" bg-gray-100" />
+                          <Button variant="outline">쿠폰적용</Button>
+                        </div>
+                      </div>
+                      <div>
+                        <p>쿠폰 번호</p>
+                        <div className="flex gap-[8px] w-[480px]">
+                          <Input className=" bg-gray-100" />
+                          <Button variant="outline">번호확인</Button>
+                        </div>
+                      </div>
+                      <div>
+                        <p>포인트</p>
+                        <div className="flex gap-[8px] w-[480px]">
+                          <Input className=" bg-gray-100" />
+                          <Button variant="outline">전액사용</Button>
+                        </div>
+                      </div>
+                      <div>
+                        <div className="flex gap-[10px] w-[480px]">
+                          <p>보유 포인트</p>
+                          <p className="font-semibold">2300</p>
+                        </div>
+                        <p>5,000 포인트 이상 보유 및 10,000원 이상 구매시 사용가능</p>
+                      </div>
+                    </div>
+                  </CardContent>
+                </Card>
 
-          <div className="flex border-t-[2px] border-gray-200/15 pt-[50px]">
-            <div className="grid w-[742px]">
-              <Card className="border-none h-[500px]">
-                <CardTitle className="text-[20px] pb-[10px]">쿠폰/포인트</CardTitle>
-                <CardContent className="grid gap-[20px] w-[700px] border-black border-t-2 px-[5px] py-[15px] items-center">
-                  <div>
-                    <p className="mb-[10px]">쿠폰</p>
-                    <Button className="hidden">
-                      <X />
-                    </Button>
-                    <div className="flex w-[480px]">
-                      <Input />
-                      <Button>쿠폰적용</Button>
-                    </div>
-                  </div>
-                  <div>
-                    <p>쿠폰 번호</p>
-                    <div className="flex w-[480px]">
-                      <Input />
-                      <Button>번호확인</Button>
-                    </div>
-                  </div>
-                  <div>
-                    <p>포인트</p>
-                    <div className="flex w-[480px]">
-                      <Input />
-                      <Button>전액사용</Button>
-                    </div>
-                  </div>
-                  <div>
-                    <div className="flex gap-[10px] w-[480px]">
-                      <p>보유 포인트</p>
-                      <p className="font-semibold">2300</p>
-                    </div>
-                    <p>5,000 포인트 이상 보유 및 10,000원 이상 구매시 사용가능</p>
-                  </div>
-                </CardContent>
-              </Card>
-
-              <Card className="grid gap-[5px] border-none">
-                <CardTitle className="text-[20px]">결제 방법</CardTitle>
-                <CardContent className="grid border-black border-t-2 px-[5px] py-[15px] items-center">
-                  <div className="grid gap-[15px] mt-[10px]">
-                    <div className="flex gap-[15px]">
-                      <div>
-                        <Checkbox />
-                        <label>신용카드</label>
+                <Card className="grid gap-[5px] border-none">
+                  <CardTitle className="text-[20px]">결제 방법</CardTitle>
+                  <CardContent className="flex w-[700px] gap-[40px] border-black border-t-2 px-[5px] py-[30px]">
+                    <label>결제 방법 선택</label>
+                    <div className="grid gap-[25px]">
+                      <div className="flex gap-[25px]">
+                        <div className="flex items-center gap-[15px]">
+                          <Checkbox />
+                          <label>신용카드</label>
+                        </div>
+                        <div className="flex items-center gap-[15px]">
+                          <Checkbox />
+                          <label>가상계좌</label>
+                        </div>
+                        <div className="flex items-center gap-[15px]">
+                          <Checkbox />
+                          <label>무통장 입금</label>
+                        </div>
+                        <div className="flex items-center gap-[15px]">
+                          <Checkbox />
+                          <label>핸드폰 결제</label>
+                        </div>
                       </div>
-                      <div>
-                        <Checkbox />
-                        <label>가상계좌</label>
+                      <div className="flex gap-[20px]">
+                        <Checkbox className="mt-[7px]" />
+                        <Card className="grid w-[450px] h-[60px] place-content-center rounded-[8px] text-lg font-semibold">
+                          <Image alt="카카오페이" src={kakaoPay} className="w-[85px] h-[26px]" />
+                        </Card>
+                      </div>
+                      <div className="flex gap-[20px]">
+                        <Checkbox className="mt-[7px]" />
+                        <Card className="grid w-[450px] h-[60px] place-content-center rounded-[8px] text-lg font-semibold">
+                          <Image alt="네이버페이" src={naverPay} className="w-[90px] h-[35px]" />
+                        </Card>
+                      </div>
+                      <div className="flex gap-[20px]">
+                        <Checkbox className="mt-[7px]" />
+                        <Card className="grid w-[450px] h-[60px] place-content-center rounded-[8px] text-lg font-semibold ">
+                          <Image alt="토스페이" src={tossPay} className="w-[150px] h-[40px]" />
+                        </Card>
                       </div>
                     </div>
-                    <div className="flex gap-[15px]">
-                      <div>
-                        <Checkbox />
-                        <label>무통장 입금</label>
-                      </div>
-                      <div>
-                        <Checkbox />
-                        <label>핸드폰 결제</label>
-                      </div>
-                    </div>
-                    <div>
+                  </CardContent>
+                </Card>
+                <Card className="grid gap-[5px] border-none">
+                  <CardTitle className="text-[20px]">개인정보 수집/제공</CardTitle>
+                  <CardContent className="grid gap-[20px] w-[700px] border-black border-t-2 px-[5px] py-[30px]">
+                    <div className="flex items-center gap-[15px]">
                       <Checkbox />
-                      <label>카카오페이</label>
+                      <p>개인정보 수집 이용 및 처리 동의</p>
                     </div>
-                  </div>
-                  <div className="border-b-2">
-                    {/* 무통장입금 시 나오는 select */}
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder="거래 은행을 선택해주세요" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectGroup>
-                          <SelectLabel>거래 은행을 선택해주세요</SelectLabel>
-                          <SelectItem value="ㅇㅇ은행: 000-00-0000 예금주명">ㅇㅇ은행: 000-00-0000 예금주명</SelectItem>
-                          <SelectItem value="xx은행">xx은행: 000-00-0000 예금주명</SelectItem>
-                          <SelectItem value="@@은행">@@은행: 000-00-0000 예금주명</SelectItem>
-                          <SelectItem value="##은행">##은행: 000-00-0000 예금주명</SelectItem>
-                        </SelectGroup>
-                      </SelectContent>
-                    </Select>
-                    <Input placeholder="입금자명 (미입력시 주문자명)" />
-                    <p>주문 후 n시간 동안 미입금시 자동 취소됩니다.</p>
-                  </div>
-                  <div className="flex gap-[15px]">
-                    <Checkbox />
-                    <label>현금영수증 신청</label>
-                  </div>
-                </CardContent>
-              </Card>
-              <Card className="border-none h-[500px]">
-                <CardTitle className="text-[20px]">
-                  <Checkbox />
-                  <label>전체 동의</label>
-                </CardTitle>
-                <CardContent>
-                  <Checkbox />
-                  <label>구매조건 확인 및 결제 진행에 동의</label>
-                </CardContent>
-                <Button>결제하기</Button>
-              </Card>
-            </div>
-            <div>
+                    <div className="flex items-center gap-[15px]">
+                      <Checkbox />
+                      <p>전자지급 결제대행 서비스 이용약관 동의</p>
+                    </div>
+                    <div className="flex items-center gap-[15px] text-xl font-semibold">
+                      <Checkbox />
+                      <p>위 내용을 확인 하였으며 결제에 동의합니다.</p>
+                    </div>
+                  </CardContent>
+                </Card>
+                <Button className="w-[320px] h-[70px] rounded-[8px] text-xl mt-[60px]">13,520원 결제하기</Button>
+              </div>
               <div>
-                <Card className="bg-gray-100 w-[300px]">
-                  <CardTitle className="ml-[27px] mb-[10px] text-[28px]">최종 결제금액</CardTitle>
-                  <CardContent className="grid gap-[20px]">
-                    <div className="flex gap-[10px] border-b-2  pb-[10px]">
-                      <p className="text-lg font-semibold">상품가격</p>
+                {/* "fixed top-[20px] grid gap-[20px] w-[300px] border-none" */}
+                <Card
+                  className={cn(
+                    IsTotalRef
+                      ? 'fixed top-[20px] grid gap-[20px] w-[300px] border-none'
+                      : 'relative grid gap-[20px] w-[300px] border-none',
+                  )}>
+                  <CardTitle className="text-[20px]">결제 금액</CardTitle>
+                  <CardContent className="grid gap-[20px] bg-gray-100 ">
+                    <div className="flex justify-between border-b-2 mt-[20px] pb-[10px]">
+                      <p className="text-lg font-semibold">주문 금액</p>
                       <div className="text-lg font-semibold">
                         <p>18,000원</p>
                       </div>
                     </div>
-                    <div>
+                    <div className="flex justify-between">
                       <p className="text-lg">쿠폰 할인</p>
-                      <div className="flex gap-[5px]">
-                        <CornerDownRight color="gray" />
-                        <p>-1000원</p>
-                      </div>
+                      <p>-1000원</p>
                     </div>
-                    <div>
+                    <div className="flex justify-between">
                       <p className="text-lg">포인트 사용</p>
-                      <div className="flex gap-[5px]">
-                        <CornerDownRight color="gray" />
-                        <p>-0원</p>
-                      </div>
+                      <p>-0원</p>
                     </div>
-                    <div>
+                    <div className="flex justify-between">
                       <p className="text-lg">배송비</p>
-                      <div className="flex gap-[5px]">
-                        <CornerDownRight color="gray" />
-                        <p>+2500</p>
-                      </div>
+                      <p>+2500</p>
                     </div>
-                    <div>
+                    <div className="flex justify-between">
                       <p className="text-lg">총 결제금액</p>
-                      <div className="flex gap-[5px]">
-                        <CornerDownRight color="gray" />
-                        <p>19,500원</p>
-                      </div>
+                      <p>19,500원</p>
                     </div>
-                    <div>
+                    <div className="flex justify-between">
                       <p className="text-lg">포인트 적립예정</p>
-                      <div className="flex gap-[5px]">
-                        <CornerDownRight color="gray" />
-                        <p>700</p>
-                      </div>
+                      <p>700</p>
                     </div>
                   </CardContent>
                 </Card>
